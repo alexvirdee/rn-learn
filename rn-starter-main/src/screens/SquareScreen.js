ea769fe3cc1c8ed4408ca4ce4b5e9f6f566e2246
Alexinder Virdee <alex.virdee08@gmail.com>
2021-09-22 13:43:36 -0400

    Passing callbacks as children

--- rn-starter-main/src/screens/SquareScreen.js
+++ rn-starter-main/src/screens/SquareScreen.js
@@ -1,18 +1,30 @@
-import React from 'react';
-import { View, Text, StyleSheet  } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import ColorCounter from '../Components/ColorCounter';
 
 const SquareScreen = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Square Screen App</Text>
-        </View>
-    )
-}
+  const [red, setRed] = useState(0);
+  const [green, setGreen] = useState(0);
+  const [blue, setBlue] = useState(0);
+  console.log(red)
+  //  
+  return (
+    <View style={styles.container}>
+      <ColorCounter
+        color="Red"
+        onIncrease={() => setRed(red + 1)}
+        onDecrease={() => setRed(red - 1)}
+      />
+      <ColorCounter color="Blue" />
+      <ColorCounter color="Green" />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-})
+  container: {
+    flex: 1,
+  },
+});
 
 export default SquareScreen;
