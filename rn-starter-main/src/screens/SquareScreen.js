10be4272213e38ff99bac9eb08ca029777776405
Alexinder Virdee <alex.virdee08@gmail.com>
2021-09-23 10:55:41 -0400

    validating state changes

--- rn-starter-main/src/screens/SquareScreen.js
+++ rn-starter-main/src/screens/SquareScreen.js
@@ -2,21 +2,47 @@ import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import ColorCounter from '../Components/ColorCounter';
 
+const COLOR_INCREMENT = 15;
+
 const SquareScreen = () => {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
-  console.log(red)
-  //  
+  //
+  const setColor = (color, change) => {
+    // color === 'red', 'green', 'blue'
+    // change === +15, -15
+
+    if (color === 'red') {
+        if (red + change > 255 || red + change < 0) {
+            return;
+        } else {
+            setRed(red + change)
+        }
+    }
+  };
+  //
   return (
     <View style={styles.container}>
       <ColorCounter
         color="Red"
-        onIncrease={() => setRed(red + 1)}
-        onDecrease={() => setRed(red - 1)}
+        onIncrease={() => {
+           setColor('red', COLOR_INCREMENT) 
+        }
+        }
+        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
       />
-      <ColorCounter color="Blue" />
-      <ColorCounter color="Green" />
+      <ColorCounter
+        color="Blue"
+        onIncrease={() => setBlue(blue + COLOR_INCREMENT)}
+        onDecrease={() => setBlue(blue - COLOR_INCREMENT)}
+      />
+      <ColorCounter
+        color="Green"
+        onIncrease={() => setGreen(green + COLOR_INCREMENT)}
+        onDecrease={() => setGreen(green - COLOR_INCREMENT)}
+      />
+      <View style={{ height: 150, width: 150, margin: 50, backgroundColor: `rgb(${red},${green},${blue})` }}></View>
     </View>
   );
 };
