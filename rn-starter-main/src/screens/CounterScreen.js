6d6d5e509d2d9e0fe23bce9006b6a25a91190188
Alexinder Virdee <alex.virdee08@gmail.com>
2021-09-23 19:15:31 -0400

    Adding reducer to counter app

--- rn-starter-main/src/screens/CounterScreen.js
+++ rn-starter-main/src/screens/CounterScreen.js
@@ -1,29 +1,47 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
-const CounterScreen = () => {
-    const [counter, setCounter] = useState(0);
+const COUNT_INCREMENT = 1;
 
-    return (
-        <View style={styles.container}>
-            <Button title="Increase" onPress={() => {
-                // Don't do this
-                // counter++;
-                setCounter(counter + 1);
-            }} />
-            <Button title="Decrease" onPress={() => {
-                setCounter(counter - 1);
-            }}/>
-            <Text>Current Count: {counter}</Text>
-        </View>
-    )
-}
+const reducer = (state, action) => {
+  // state === { count: number }
+  // action === { type: 'increment' || 'decrement', payload }
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + action.payload };
+    case 'decrement':
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+
+const CounterScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  //
+  return (
+    <View style={styles.container}>
+      <Button
+        title="Increase"
+        onPress={() => {
+          dispatch({ type: 'increment', payload: COUNT_INCREMENT });
+        }}
+      />
+      <Button
+        title="Decrease"
+        onPress={() => {
+          dispatch({ type: 'decrement', payload: COUNT_INCREMENT });
+        }}
+      />
+      <Text>Current Count: {state.count}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
-})
-
+  container: {
+    flex: 1,
+  },
+});
 
 export default CounterScreen;
