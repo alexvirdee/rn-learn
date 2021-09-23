3531d95b9d18fc66256e790007133e29ebdb1362
Alexinder Virdee <alex.virdee08@gmail.com>
2021-09-23 11:54:54 -0400

    Restoring validation utilizing a reducer

--- rn-starter-main/src/screens/SquareScreen.js
+++ rn-starter-main/src/screens/SquareScreen.js
@@ -12,11 +12,17 @@ const reducer = (state, action) => {
     case 'red':
       // Never do this
       // state.red = state.red - 15
-      return { ...state, red: state.red + action.amount };
-    case 'blue':
-      return { ...state, blue: state.blue + action.amount };
+      return state.red + action.amount > 255 || state.red < 0 
+        ? state
+        : { ...state, red: state.red + action.amount }
     case 'green':
-      return { ...state, green: state.green + action.amount };
+      return state.green + action.amount > 255 || state.green < 0 
+        ? state
+        : { ...state, green: state.green + action.amount }
+    case 'blue':
+      return state.blue + action.amount > 255 || state.blue < 0 
+        ? state
+        : { ...state, blue: state.blue + action.amount }
     default:
       return state;
   }
